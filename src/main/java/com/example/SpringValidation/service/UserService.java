6c52ae354a3c47e0e1b72b1faf361b613821dcbc
Nikko <nickouchiha5@gmail.com>
2022-03-18 09:36:58 +0800

    Spring-Validation

---
+++ src/main/java/com/example/SpringValidation/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.SpringValidation.service;
+
+import com.example.SpringValidation.model.User;
+import com.example.SpringValidation.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
